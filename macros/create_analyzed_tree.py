2d48941d725bb92dd999aa7190afa49b9fb176cc
Kirsten Randle <kirstenrandle@Kirstens-MacBook-Pro.local>
2019-12-04 20:41:35 -0500

    Got both layers running, updated outputs to reflect running state

--- macros/create_analyzed_tree.py
+++ macros/create_analyzed_tree.py
@@ -10,7 +10,7 @@ out_dir = '/Users/kirstenrandle/CMS_work/compphysproj/outputs/'
 file_1 = in_dir + 'Timing_bias_rings_etr28_PU50_S1.99_2.01.root'
 file_2 = in_dir + 'Timing_samplesandweights.root'
 
-out_file = out_dir + 'analyzed_tree_1.root'
+out_file = out_dir + 'analyzed_tree.root'
 
 #open files and load tree
 df1 = ROOT.TFile.Open(file_1)
@@ -44,7 +44,9 @@ weights_compare_list = [['w_{}_compare'.format(i),'Timing weight vs. amplitude w
 """
 Define functions for plotting, iteratively if needed
 """
+gROOT.SetBatch(1) #don't output on Draw function
 
+#Use root to create a 1-D histogram from parameters set above
 def create_1Dhisto(tree_,hname_,title_,binparams_,parameter_,cuts_):
     h = ROOT.TH1F(hname_,title_,binparams_[0],binparams_[1],binparams_[2])
     h.GetXaxis().SetTitle(parameter_)
@@ -54,6 +56,7 @@ def create_1Dhisto(tree_,hname_,title_,binparams_,parameter_,cuts_):
     h.SetDirectory(0)
     return h
 
+#Use root to create a 2-D histogram from parameters set above
 def create_2Dhisto(tree_,hname_,title_,binparams_,parameters_,cuts_):
     h = ROOT.TH2F(hname_,title_,binparams_[0][0],binparams_[0][1],binparams_[0][2],binparams_[1][0],binparams_[1][1],binparams_[1][2])
     h.GetXaxis().SetTitle(parameters_[0])
@@ -63,38 +66,80 @@ def create_2Dhisto(tree_,hname_,title_,binparams_,parameters_,cuts_):
     h.SetDirectory(0)
     return h
 
+class HistogramWrongDimensions(Exception):
+    pass
+
+def means_of_several(hlist_,hname_,title_):
+    nbins = len(hlist_)
+    lowbin = -0.5
+    highbin = lowbin+nbins
+    newhist = ROOT.TH1F(hname_,title_,nbins,lowbin,highbin)
+    for i,h in enumerate(hlist_):
+        if h.ClassName() != 'TH1F':
+            print('means_of_several only works with 1-D histograms at the moment')
+            raise HistogramWrongDimensions
+        else:
+            newhist.SetBinContent(i+1,h.GetMean())
+            newhist.SetBinError(i+1,h.GetStdDev())
+    newhist.SetDirectory(0)
+    return newhist
+            
+#take vertical slices and fit each with a gaussian and report mean and stdev
 def slicefity(histo_,func_,slicebins_,options_):
+    #slicebins are xlow,xhigh,nbins
     fitparams = ROOT.TObjArray()
-    histo.FitSlicesY(func_,slicebins_[0],slicebins_[1],slicebins_[2],options_,fitparams)
+    histo_.FitSlicesY(func_,slicebins_[0],slicebins_[1],slicebins_[2],options_,fitparams)
     return fitparams
 
+#use list of inputs to create many histograms
 def iterate_curves(tree_,curvelist_,type_):
     list = [0 for i in range(len(curvelist_))]
     for i,p in enumerate(curvelist_):
         if type_ == 'TH1F':
             list[i] = create_1Dhisto(tree_,p[0],p[1],p[2],p[3],p[4])
         if type_ == 'TH2F':
-            list[i]= create_2Dhisto(tree_,p[0],p[1],p[2],p[3],p[4])
+            list[i]= [create_2Dhisto(tree_,p[0],p[1],p[2],p[3],p[4])]
+            fp = slicefity(list[i][0],0,[0,-1,0],'Q') #for every 2D histo report stats for slices
+            list[i]+= [fp.At(1)]
+            list[i]+= [fp.At(2)]
     return list
-            
 
+
+
+#create all the histograms specified above
 reco_histos = iterate_curves(bt1,reco_list,'TH1F')
 digis_histos = iterate_curves(bt2,digis_list,'TH1F')
+avg_digis = means_of_several(digis_histos[:10],'avg_digis','Average Total Digis Values')
+avg_p_digis = means_of_several(digis_histos[10:20],'avg_pileup_digis','Average Pileup Digis Values')
+avg_n_digis = means_of_several(digis_histos[20:30],'avg_noise_digis','Average Noise Digis Values')
+avg_s_digis = means_of_several(digis_histos[30:40],'avg_signal_digis','Average Signal Digis Values')
 weights_histos = iterate_curves(bt2,weights_list,'TH1F')
+avg_weights_1 = means_of_several(weights_histos[:5],'avg_weights_1','Average Amplitude Weights')
+avg_weights_2 = means_of_several(weights_histos[5:],'avg_weights_2','Average Timing Weights')
 weights_compare_histos = iterate_curves(bt2,weights_compare_list,'TH2F')
 recoa_compare_histos = iterate_curves(bt1,recoa_compare_list,'TH2F')
 
+#write all the histograms to a root tree for later processing
 of = ROOT.TFile(out_file,'RECREATE')
-
 for h in reco_histos:
     h.Write()
 for h in digis_histos:
     h.Write()
 for h in weights_histos:
     h.Write()
-for h in weights_compare_histos:
-    h.Write()
-for h in recoa_compare_histos:
-    h.Write()
+
+for l in weights_compare_histos: #save regular 2D histogram and statistics
+    for h in l:
+        h.Write()
+
+for l in recoa_compare_histos:
+    for h in l:
+        h.Write()
+avg_digis.Write()
+avg_p_digis.Write()
+avg_n_digis.Write()
+avg_s_digis.Write()
+avg_weights_1.Write()
+avg_weights_2.Write()
 
 of.Close()
--- macros/plot_analyzed_tree.py
+++ macros/plot_analyzed_tree.py
@@ -4,31 +4,130 @@
 import ROOT
 from array import array
 
-dir = '/Users/kirstenrandle/CMS_work/compphysproj/data/'
+dir = '/Users/kirstenrandle/CMS_work/compphysproj/outputs/'
 digis_dir = dir + 'digis/'
-infile = dir + 'analyzed_tree_1.root'
+weights_dir = dir + 'weights/'
+infile = dir + 'analyzed_tree.root'
 
 hf = ROOT.TFile.Open(infile)
 
-avg_digis = ROOT.TH1F('avg_digis','Average digi values',10,-0.5,9.5)
-avg_pileup_digis = ROOT.TH1F('avg_digis','Average digi values',10,-0.5,9.5)
 
 for i in range(10):
     c = ROOT.TCanvas('c','c',600,800)
+    ROOT.gStyle.SetOptStat(0)
     digi = hf.Get('digis_{}'.format(i))
+    digi.GetYaxis().SetTitleOffset(1)
     digi.Draw('hist')
     c.SaveAs(digis_dir + 'digis_{}.png'.format(i))
     d = ROOT.TCanvas('d','d',600,800)
+    ROOT.gStyle.SetOptStat(0)
     p_digi = hf.Get('pileup_digis_{}'.format(i))
     p_digi.Draw('hist')
     d.SaveAs(digis_dir + 'pileup_digis_{}.png'.format(i))
     e = ROOT.TCanvas('e','e',600,800)
+    ROOT.gStyle.SetOptStat(0)
     n_digi = hf.Get('noise_digis_{}'.format(i))
     n_digi.Draw('hist')
     e.SaveAs(digis_dir + 'noise_digis_{}.png'.format(i))
     f = ROOT.TCanvas('f','f',600,800)
+    ROOT.gStyle.SetOptStat(0)
     s_digi = hf.Get('signal_digis_{}'.format(i))
     s_digi.Draw('hist')
     f.SaveAs(digis_dir + 'signal_digis_{}.png'.format(i))
-    
-    
+
+
+
+digicolors = array('i',[ROOT.kBlack,ROOT.kGreen,ROOT.kMagenta,ROOT.kRed])
+
+c1 = ROOT.TCanvas('c1','c1',600,800)
+ROOT.gStyle.SetPalette(4,digicolors)
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetErrorX(0.)
+
+avd = hf.Get('avg_digis')
+avsd = hf.Get('avg_signal_digis')
+avnd = hf.Get('avg_noise_digis')
+avpd = hf.Get('avg_pileup_digis')
+
+avd.GetYaxis().SetTitle('Avg. Amplitude')
+avd.GetXaxis().SetTitle('Sample number')
+
+avd.SetMarkerStyle(21)
+avnd.SetMarkerStyle(21)
+avpd.SetMarkerStyle(21)
+avsd.SetMarkerStyle(21)
+
+avd.Draw('eSAME PLC PMC')
+avsd.Draw('eSAME PLC PMC')
+avnd.Draw('eSAME PLC PMC')
+avpd.Draw('eSAME PLC PMC')
+
+avd.GetYaxis().SetRangeUser(-5,40)
+l1 = ROOT.TLegend(0.1,0.7,0.3,0.9)
+l1.AddEntry(avd)
+l1.AddEntry(avsd)
+l1.AddEntry(avnd)
+l1.AddEntry(avpd)
+l1.Draw()
+c1.SaveAs(dir + 'avg_digis_all.png')
+
+
+for i in range(5):
+    c = ROOT.TCanvas('c','c',2400,800)
+    ROOT.gStyle.SetOptStat(0)
+    ROOT.gStyle.SetPalette(ROOT.kBird)
+    c.Divide(3,1)
+    c.cd(1)
+    w1 = hf.Get('w_1_{}'.format(i))
+    w1.SetTitleOffset(1)
+    w1.Draw('hist')
+    c.cd(2)
+    w2 = hf.Get('w_2_{}'.format(i))
+    w2.SetTitleOffset(1)
+    w2.Draw()
+    c.cd(3)
+    wvs = hf.Get('w_{}_compare'.format(i))
+    wvs.GetYaxis().SetTitleOffset(1)
+    wvs.Draw('COLZ')
+    c.SaveAs(weights_dir + 'w_{}_compare.png'.format(i))
+
+c2 = ROOT.TCanvas('c2','c2',600,800)
+ROOT.gStyle.SetPalette(2,digicolors[1:3])
+ROOT.gStyle.SetOptStat(0)
+ROOT.gStyle.SetErrorX(0.)
+avg_w1 = hf.Get('avg_weights_1')
+avg_w2 = hf.Get('avg_weights_2')
+avg_w1.SetMarkerStyle(21)
+avg_w2.SetMarkerStyle(21)
+avg_w1.Draw('eSAME PLC PMC')
+avg_w2.Draw('eSAME PLC PMC')
+avg_w1.GetYaxis().SetRangeUser(-1,1)
+l2 = ROOT.TLegend(0.7,0.1,0.9,0.3)
+l2.AddEntry(avg_w1)
+l2.AddEntry(avg_w2)
+l2.Draw()
+c2.SaveAs(dir + 'avg_weights.png')
+
+c3 = ROOT.TCanvas('c3','c3',1600,800)
+ROOT.gStyle.SetPalette(ROOT.kBird)
+ROOT.gStyle.SetOptStat(0)
+rA1vrA2 = hf.Get('recoA_2_vs_recoA_1')
+rA1vrA2av = hf.Get('recoA_2_vs_recoA_1_1')
+c3.Divide(2,1)
+c3.cd(1)
+rA1vrA2.Draw('COLZ')
+c3.cd(2)
+rA1vrA2av.Draw()
+c3.SaveAs(dir+'recoA_compare.png')
+
+c4 = ROOT.TCanvas('c4','c4',1600,800)
+ROOT.gStyle.SetPalette(ROOT.kBird)
+ROOT.gStyle.SetOptStat(0)
+b1vrA2 = hf.Get('recoA_2_vs_bias_1')
+b1vrA2av = hf.Get('recoA_2_vs_bias_1_1')
+c4.Divide(2,1)
+c4.cd(1)
+b1vrA2.Draw('COLZ')
+c4.cd(2)
+b1vrA2av.Draw()
+c4.SaveAs(dir+'recoA_bias_compare.png')
